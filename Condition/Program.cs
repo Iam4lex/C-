176e74aeadf0a2cd4be95a1136f5f122aa8b07a8
Alex Mwangi <mungaialexm@gmail.com>
2024-05-01 11:11:18 +0300

    first commit

---
+++ Condition/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Enter the any number");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("The number is " + number);
+
+Console.WriteLine(number > 0 ? "The number is greater tha 0" : "The number is less than 0");
+
