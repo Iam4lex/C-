52faf07658f11fd44b3f0b464e5de1e2b2c3102e
Alex Mwangi <mungaialexm@gmail.com>
2024-05-01 10:34:55 +0300

    first commit

--- Foreach/obj/Debug/net8.0/Foreach.AssemblyInfo.cs
+++ Foreach/obj/Debug/net8.0/Foreach.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Foreach")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9c81335a29fe45bfca2a9b8f770d062903d5135")]
 [assembly: System.Reflection.AssemblyProductAttribute("Foreach")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Foreach")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
