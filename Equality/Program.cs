63098c914aef98a4f47f09c6abe249e042a60079
Alex Mwangi <mungaialexm@gmail.com>
2024-05-01 10:45:53 +0300

    first commit

---
+++ Equality/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("a" == "a");
+Console.WriteLine("a" == "A");
+Console.WriteLine(1 == 2);
+
+string myValue = "a";
+Console.WriteLine(myValue == "a");
+
+string value1 = " a";
+string value2 = "A ";
+Console.WriteLine(value1.Trim().ToLower() == value2.Trim().ToLower());
+
+string pangram = "The quick brown fox jumps over the lazy dog.";
+Console.WriteLine(pangram.Contains("fox"));
+Console.WriteLine(pangram.Contains("cow"));
