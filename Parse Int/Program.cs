d1046cdda371583d44c246157de746f0b685f60f
Alex Mwangi <mungaialexm@gmail.com>
2024-05-06 12:55:44 +0300

    array of pallets

--- Parse Int/Program.cs
+++ Parse Int/Program.cs
@@ -1,1 +1,10 @@
-﻿
+﻿string value = "102";
+int result = 0;
+if (int.TryParse(value, out result))
+{
+   Console.WriteLine($"Measurement: {result}");
+}
+else
+{
+   Console.WriteLine("Unable to report the measurement.");
+}
