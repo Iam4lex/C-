f063acac014e50377ea3771272cdf95c24283c38
Alex Mwangi <mungaialexm@gmail.com>
2024-05-06 13:46:43 +0300

    Code

--- array of pallets/Program.cs
+++ array of pallets/Program.cs
@@ -1,11 +1,15 @@
 ﻿string[] pallets = { "B14", "A11", "B12", "A13" };
 
+// Sort the array elemnts
+
 Console.WriteLine("Sorted...");
 Array.Sort(pallets);
 foreach (var pallet in pallets)
 {
     Console.WriteLine($"-- {pallet}");
 }
+
+// Reverse the array Elements
 
 Console.WriteLine("");
 Console.WriteLine("Reversed...");
@@ -14,3 +18,16 @@ foreach (var pallet in pallets)
 {
     Console.WriteLine($"-- {pallet}");
 }
+
+// Clear the elements of the array
+
+Array.Clear(pallets, 0, 2);
+Console.WriteLine($"Clearing 2 ... count: {pallets.Length}");
+foreach (var pallet in pallets)
+{
+    Console.WriteLine($"-- {pallet}");
+}
+
+Console.WriteLine($"Before: {pallets[0]}");
+Array.Clear(pallets, 0, 2);
+Console.WriteLine($"After: {pallets[0]}");
